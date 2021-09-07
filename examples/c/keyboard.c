d6cc4dc7c762be9fe304aacb756cc100c48196ec
shokre <kbrlek@gmail.com>
2021-09-07 23:41:53 +0200

    feat: add keyboard example

---
+++ examples/c/keyboard.c
@@ -0,0 +1,23 @@
+#include <orao.h>
+
+void main() {
+    char c;
+
+    orao_cls();
+    orao_puts("ORAO cc65 == Keyboard test");
+    orao_print_newline();
+    orao_putc('\n');
+    orao_puts("Press any key to see ASCII code value. Press <Q> to quit.");
+    orao_print_newline();
+    orao_putc('\n');
+    asm("nop");
+    do {
+    c = orao_getc();
+    orao_print_space_hex(c);
+    orao_putc(':');
+    orao_putc(c);
+    } while(c != 'Q');
+    orao_print_newline();
+    orao_puts("END");
+    orao_print_newline();
+}
