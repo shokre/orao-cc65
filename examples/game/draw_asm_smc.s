e002722b90bf1377071de4b3361ab8fef110a60f
shokre <kbrlek@gmail.com>
2021-11-20 14:25:54 +0100

    feat: slightly optimize map drawing routine (-347cy)

--- examples/game/draw_asm_smc.s
+++ examples/game/draw_asm_smc.s
@@ -16,6 +16,7 @@
 ; counters
 draw_width: .res 1
 draw_height: .res 1
+scanline_cnt: .res 1
 
 ; pointers
 draw_pos_lo: .res 1
@@ -38,7 +39,7 @@ draw_pos_lo: .res 1
     rts
 .endproc
 
-; draw map
+; draw map 62992cy (for 20x20 area)
 .proc _draw_asm_smc_map
     ; set low byte of tiles_start to 0 because tiles are page aligned
     lda #0
@@ -53,70 +54,34 @@ draw_pos_lo: .res 1
     dex
     stx draw_width
 
-_line_loop:
-    jsr _draw_map_row
+    ; start drawing
+    jmp _row_loop
 
+; this is ignored in first step
+_draw_next_line:
     ; advance map to next row
     SMC_AddAddrLo MapStartPos, _map_data_width
-    bcc _skip
+    bcc __skip
     SMC_OperateOnHighByte INC, MapStartPos
-_skip:
+__skip:
 
     ; advance draw position onto next page
     SMC_OperateOnHighByte INC, DrawStartPos
-
     ; reset low byte
     SMC_TransferLowByte DrawStartPos, draw_pos_lo
 
-    ; decrement height counter
-    dec draw_height
-    ; if not zero _line_loop
-    bne _line_loop
-    rts
-.endproc
-
-.macro _advance_draw_ptrs_scanline
-    ; inc just the lo byte because we are on same page
-    SMC_AddAddrLo DrawStartPos, #32
-
-    ; inc tile scanline
-    SMC_OperateOnHighByte INC, TilesStartPos
-.endmacro
-
-; draw a map row of 8 scanlines
-.proc _draw_map_row
+_row_loop:
     ; init tiles location
     lda #>_gfx_data_tiles
     SMC_StoreHighByte TilesStartPos, A
 
-    jsr _draw_map_scanline
-    ;; draw next line
-    _advance_draw_ptrs_scanline
-    jsr _draw_map_scanline
-    ;; draw next line
-    _advance_draw_ptrs_scanline
-    jsr _draw_map_scanline
-    ;; draw next line
-    _advance_draw_ptrs_scanline
-    jsr _draw_map_scanline
-    ;; draw next line
-    _advance_draw_ptrs_scanline
-    jsr _draw_map_scanline
-    ;; draw next line
-    _advance_draw_ptrs_scanline
-    jsr _draw_map_scanline
-    ;; draw next line
-    _advance_draw_ptrs_scanline
-    jsr _draw_map_scanline
-    ;; draw next line
-    _advance_draw_ptrs_scanline
-    jmp _draw_map_scanline
-.endproc
+    ; draw 8 scanlines
+    ldx #8
+    stx scanline_cnt
 
-; draw current scanline
-.proc _draw_map_scanline
+_draw_map_scanline:
     ldx draw_width
-_draw:    
+_scanline_loop:
     ; load map byte
     SMC MapStartPos, { ldy SMC_AbsAdr,x }
     ; load tile byte from map byte
@@ -125,10 +90,28 @@ _draw:
     SMC DrawStartPos, { sta SMC_AbsAdr,x }
 
     dex
-    bpl _draw
+    bpl _scanline_loop
+
+    ; next scanline
+    dec scanline_cnt ; 5cy
+    beq _finish_draw_scanline     ; 2cy
+
+    ; inc just the lo byte because we are on the same page
+    SMC_AddAddrLo DrawStartPos, #32 ; 10cy
+
+    ; inc tile scanline
+    SMC_OperateOnHighByte INC, TilesStartPos ; 6cy
+
+    jmp _draw_map_scanline   ; 3cy
+
+_finish_draw_scanline:
+
+    ; decrement height counter
+    dec draw_height
+    bne _draw_next_line
     rts
 .endproc
 
-SMC_Export MapStartPos, _draw_map_scanline::MapStartPos
-SMC_Export TilesStartPos, _draw_map_scanline::TilesStartPos
-SMC_Export DrawStartPos, _draw_map_scanline::DrawStartPos
+SMC_Export MapStartPos, _draw_asm_smc_map::MapStartPos
+SMC_Export TilesStartPos, _draw_asm_smc_map::TilesStartPos
+SMC_Export DrawStartPos, _draw_asm_smc_map::DrawStartPos
