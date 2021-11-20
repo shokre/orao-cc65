826cceecd6a441128edb6e3a61051f84f3cda15d
shokre <kbrlek@gmail.com>
2021-11-20 14:31:45 +0100

    chore: use da65 for disassembly

--- examples/shared.mk
+++ examples/shared.mk
@@ -4,6 +4,7 @@ RUNTIME_DIR = ../../libsrc
 LIB_FILE = $(RUNTIME_DIR)/orao.lib
 INCLUDE_FLAGS = -I $(RUNTIME_DIR)/orao
 DXA_BIN = ../../../dxa-0.1.4/dxa
+MAP2INFO_BIN = ../../utils/map2info.py
 
 clean-lib:
 	cd $(RUNTIME_DIR); make clean all
